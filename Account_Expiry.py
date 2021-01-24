6d27c559adea6190a81856a4d28766c2aa7e3992
Vikram Khosa <vskhosa@gmail.com>
2021-01-24 12:59:18 -0500

    Script to read account expiry users, increase the timestamp by 30 days, convert to LDAP format.

---
+++ Account_Expiry.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from tkinter import filedialog
+import csv
+import pandas as pd
+import datetime
+
+root = tk.Tk()
+root.withdraw()
+
+#Input file from user
+file1 = filedialog.askopenfilename(title = "Select Account Expiry File")
+
+#file1 = 'Soon-to-expire User Accounts.csv'
+
+df=pd.read_csv(file1,encoding='ISO-8859-1')
+
+#Delete rows where user is Disabled or lognHours are none
+df = df[df.userAccountControl != "Disabled"]
+df = df[df.logonHours != '0']
+
+#Convert accountExpires column to date data type
+df["accountExpires"]= pd.to_datetime(df["accountExpires"])
+
+#Add 30 days to date
+df['accountExpires'] = df['accountExpires'] + pd.DateOffset(days=30)
+#Convert date to LDAP 18-digit timestamp
+df['accountExpires'] = df['accountExpires'].apply(lambda x: (((x-datetime.datetime(1970,1,1)).total_seconds())+11644473600)*10000000)
+
+#Drop unwanted columns
+df.drop(df.columns[0], axis=1, inplace=True)
+df.drop(['userAccountControl', 'logonHours'], axis=1, inplace=True)
+
+#Write file to a csv. Using float format to avoid scientific notation for LDAP date
+df.to_csv('Account_ExpiryNEW.csv',index=False,float_format='%.0f')
